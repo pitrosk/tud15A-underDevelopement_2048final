af9382fa894f82989b8f2c42f936c2822731e6ff
Kubra <kcengiz@itu.edu.tr>
2017-03-15 15:58:56 +0100

    * Added Cells and Matrix classes. * Added Singleton Pattern for Cells class. * Changed dimension values more generic.

--- TUD15A-underDevelopent_project/src/ned/tud15a/underDevelopment/ActionRight.java
+++ TUD15A-underDevelopent_project/src/ned/tud15a/underDevelopment/ActionRight.java
@@ -3,14 +3,17 @@ package ned.tud15a.underDevelopment;
 import java.awt.event.KeyEvent;
 
 public class ActionRight implements Action {
-	int cells [][];
-	public ActionRight(int[][] matrix) {
-		cells = matrix;
+	Cells main_cells = Cells.getInstance();
+	int[][] cells = main_cells.matrix.data;
+	int r = main_cells.matrix.data.length;
+	int c = main_cells.matrix.data[0].length;
+	public ActionRight() {
+
 	}
 
 	private void merge() {
-		for (int j = 0; j < 4; j++) {
-			for (int i = 3; i > 0; i--) {
+		for (int j = 0; j < c; j++) {
+			for (int i = r-1; i > 0; i--) {
 				if (cells[i][j] == cells[i - 1][j]) {
 					cells[i][j] = 2 * cells[i][j];
 					cells[i - 1][j] = 0;
@@ -20,17 +23,17 @@ public class ActionRight implements Action {
 	}
 
 	private void shift() {
-		for (int j = 0; j < 4; j++) {
-			int[] tab = { 0, 0, 0, 0 };
+		for (int j = 0; j < cells[0].length; j++) {
+			int[] tab = new int[cells.length];
 			int index = 0;
-			for (int i = 3; i > -1; i--) {
+			for (int i = cells.length-1; i > -1; i--) {
 				if (cells[i][j] != 0) {
 					tab[index] = cells[i][j];
 					index++;
 				}
 			}
-			for (int p = 3; p > -1; p--) {
-				cells[p][j] = tab[3 - p];
+			for (int p = cells.length-1; p > -1; p--) {
+				cells[p][j] = tab[cells[0].length-1 - p];
 			}
 
 		}
---
+++ TUD15A-underDevelopent_project/src/ned/tud15a/underDevelopment/Cells.java
@@ -0,0 +1,20 @@
+package ned.tud15a.underDevelopment;
+
+
+public class Cells {
+
+    public final Matrix matrix;
+    private static Cells uniqueInstance;
+
+    public Cells() {
+        matrix = new Matrix();
+    }
+
+    public static Cells getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Cells();
+        }
+        return uniqueInstance;
+    }
+    // other useful methods here
+}
